c58f1ec08531a8735349b2106b16bb7bada734a1
larscom <larskniep@gmail.com>
2022-12-04 17:16:12 +0100

    removed modules

--- web/app/modules/dashboard/dashboard.module.ts
+++ web/app/modules/dashboard/dashboard.module.ts
@@ -1,7 +1,10 @@
-import { MaterialModule } from '@/app/shared/material.module'
+import { MaterialModule } from '@/shared/material.module'
+import { PipesModule } from '@/shared/pipes/pipes.module'
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { GroupTabsModule } from './components/group-tabs/group-tabs.module'
+import { GroupTabsComponent } from './components/group-tabs/group-tabs.component'
+import { PipelineStatusTabsComponent } from './components/pipeline-status-tabs/pipeline-status-tabs.component'
+import { ProjectTableComponent } from './components/project-table/project-table.component'
 import { DashboardRoutingModule } from './dashboard-routing.module'
 import { DashboardPage } from './dashboard.page'
 import { DashboardStore } from './dashboard.store'
@@ -9,13 +12,13 @@ import { GroupService } from './services/group.service'
 import { ProjectService } from './services/project.service'
 
 @NgModule({
-  declarations: [DashboardPage],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule,
-    GroupTabsModule,
-    MaterialModule,
+  declarations: [
+    DashboardPage,
+    ProjectTableComponent,
+    PipelineStatusTabsComponent,
+    GroupTabsComponent,
   ],
+  imports: [CommonModule, DashboardRoutingModule, MaterialModule, PipesModule],
   providers: [DashboardStore, GroupService, ProjectService],
 })
 export class DashboardModule {}
