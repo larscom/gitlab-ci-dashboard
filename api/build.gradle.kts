ac8ec68400f2aeadfc7ed1b238149f27eab25172
larscom <larskniep@gmail.com>
2023-01-18 22:34:20 +0100

    add plugin

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.7"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("com.adarshr.test-logger") version "3.2.0"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
 }
