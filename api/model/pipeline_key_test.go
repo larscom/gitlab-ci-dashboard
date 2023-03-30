dc2340cc155aec565df47e080783ef3959ed03de
larscom <larskniep@gmail.com>
2023-03-30 21:01:57 +0200

    add test

--- api/model/pipeline_key_test.go
+++ api/model/pipeline_key_test.go
@@ -6,11 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPipelineKey(t *testing.T) {
+func TestParse(t *testing.T) {
 	key := NewPipelineKey(1, "master")
 	projectId, ref := key.Parse()
 
 	assert.Equal(t, "1@master", string(key))
 	assert.Equal(t, 1, projectId)
 	assert.Equal(t, "master", ref)
+}
+
+func TestParsePanicLength(t *testing.T) {
+	key := PipelineKey("master")
+	assert.PanicsWithValue(t, "unexpected length", func() { key.Parse() })
+}
+
+func TestParsePanicProjectId(t *testing.T) {
+	key := PipelineKey("nan@master")
+	assert.PanicsWithValue(t, "could not parse", func() { key.Parse() })
 }
