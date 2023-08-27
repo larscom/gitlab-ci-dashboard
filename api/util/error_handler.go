4ccc24b52e7d26d54c7da0ee7d5214386bd798f1
larscom <larskniep@gmail.com>
2023-08-27 10:56:12 +0200

    to server error

--- api/util/error_handler.go
+++ api/util/error_handler.go
@@ -8,21 +8,25 @@ import (
 
 func HandleError[T any](value T, r *gitlab.Response, err error) (T, *gitlab.Response, error) {
 	if r == nil {
-		return value, nil, fiber.NewError(fiber.StatusGatewayTimeout, "no response from gitlab")
+		const msg = "no response from gitlab"
+		log.Debug(msg)
+		return value, nil, fiber.NewError(fiber.StatusGatewayTimeout, msg)
 	}
 
 	switch r.StatusCode {
 	case fiber.StatusUnauthorized:
 		const msg = "access token is invalid or has expired"
 		log.Debug(msg)
-		err = fiber.NewError(r.StatusCode, msg)
+		err = fiber.NewError(fiber.StatusInternalServerError, msg)
 	case fiber.StatusForbidden:
 		err = nil
 	case fiber.StatusNotFound:
 		log.Debug("requested resource could not be found: ", r.Request.URL)
 		err = nil
 	default:
-		err = fiber.NewError(r.StatusCode, err.Error())
+		msg := err.Error()
+		log.Debug(msg)
+		err = fiber.NewError(fiber.StatusInternalServerError, msg)
 	}
 
 	return value, r, err
